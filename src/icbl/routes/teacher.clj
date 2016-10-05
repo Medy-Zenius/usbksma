c08494e23dc473c087bd04a6156411c313f03d03
Medy-Zenius <medy@zeniuseducation.com>
2016-10-05 14:41:01 +0700

    can now bulk upload

--- src/icbl/routes/teacher.clj
+++ src/icbl/routes/teacher.clj
@@ -124,8 +124,10 @@
 
 (defn handle-teacher-upload [id kode file]
   (do
-    (for [i file]
-      (io/upload-file (str "resources/public/proset/" id "/" kode) i))
+    (if (vector? file)
+      (doseq [i file]
+          (io/upload-file (str "resources/public/proset/" id "/" kode) i))
+      (io/upload-file (str "resources/public/proset/" id "/" kode) file))
     (layout/render "teacher/upload.html" {:kode kode})))
 
 (defn teacher-buat-kunci [kode]
