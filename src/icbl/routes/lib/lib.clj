e4171146e45a825695f7f1e568ff0675e29a4150
Medy-Zenius <medy@zeniuseduction.com>
2016-10-14 13:50:20 +0700

    rapor

--- src/icbl/routes/lib/lib.clj
+++ src/icbl/routes/lib/lib.clj
@@ -1,5 +1,6 @@
 (ns icbl.routes.lib.lib)
 
+;;Parse data
 (def data (slurp "C:/Users/medy/Desktop/SOAL.txt"))
 
 (defn p-dat [dat]
@@ -15,3 +16,19 @@
                                           (= (nth % 1) \.) 
                                           (= (nth % 2) \space)))
                                 (p-dat dat)))))
+
+;;Script Rapor
+(def name "Suparjo")
+(def m-nilai {:subject "Fisika" :nilai "98"})
+(def l-nilai {:subject "Bahasa Inggris" :nilai "56"})
+(def bla-nilai {:subject "Kesenian" :nilai "46"})
+(def ala-nilai {:subject "Matematika" :nilai "86"})
+
+(defn rapor [mn ln al bl]
+  (str name " unggul dalam pelajaran " (:subject mn) 
+       " dengan nilai " (:nilai mn) ". Namun dalam pelajaran " 
+       (:subject ln) ", dia meraih nilai yang rendah dengan nilai "
+       (:nilai ln) ". Dia meraih nilai " (:subject al) " yang diatas rata-rata kelas."
+       " Namun masih perlu bekerja lebih giat lagi dalam pelajaran " 
+       (:subject bla-nilai)))
+
